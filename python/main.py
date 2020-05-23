e4035b0a3d15810e004973414a2118dd80a20afb
ckear1989 <ckear1989@gmail.com>
2020-05-23 11:54:58 +0100

    user input

--- python/main.py
+++ python/main.py
@@ -1,3 +1,15 @@
 
-print 'hello'
+def choose_option(txt):
+  if txt == 'new':
+    print txt
+  elif txt == 'exit':
+    print 'bye'
+    exit()
+
+if __name__=="__main__":
+
+  print 'hello'
+  while True:
+    a = raw_input()
+    choose_option(a)
 
