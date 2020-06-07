42bb05e60af98506cb907764880193b9f6f8e1fe
ckear1989 <ckear1989@gmail.com>
2020-06-07 09:40:05 +0100

    cap and floor player attributes

--- python/player.py
+++ python/player.py
@@ -4,13 +4,16 @@ import random
 
 import default
 
+def random_0_100_normal(mean, sd):
+  return int(max(min(round(np.random.normal(mean, sd), 0), 100), 0))
+
 class Player():
   def __init__(self):
     self.first_name = random.choice(default.first_names)
     self.last_name = random.choice(default.last_names)
-    self.passing = int(round(np.random.normal(80, 20), 0))
-    self.shooting = int(round(np.random.normal(70, 20), 0))
-    self.fitness = int(round(np.random.normal(70, 20), 0))
+    self.passing = random_0_100_normal(80, 20)
+    self.shooting = random_0_100_normal(70, 20)
+    self.fitness = random_0_100_normal(70, 20)
     self.overall = int(round(np.mean([
       self.passing,
       self.shooting,
