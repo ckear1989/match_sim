889c8d937e6a1a93b86e66a4a3e3523c995734c2
ckear1989 <ckear1989@gmail.com>
2020-06-12 11:08:17 +0100

    remove hardcoded names from default

--- python/default.py
+++ python/default.py
@@ -2,12 +2,6 @@
 with open('../data/defaults/poss_teams.txt', 'r') as f:
   poss_teams = [x.strip() for x in f.readlines()]
 
-with open('../data/defaults/first_names.txt', 'r') as f:
-  first_names = [x.strip() for x in f.readlines()]
-
-with open('../data/defaults/last_names.txt', 'r') as f:
-  last_names = [x.strip() for x in f.readlines()]
-
 dow = {
   'monday': 0,
   'mo': 0,
