c708c8c6b22c80582a8f85fbe56e7cc1458e6cf4
ckear1989 <ckear1989@gmail.com>
2020-06-13 09:30:30 +0100

    formations

---
+++ python/formation.py
@@ -0,0 +1,44 @@
+
+from default import formations
+
+class Formation():
+  def __init__(self):
+    self.nlist = formations[0]
+    self.get_pos()
+    self.get_pos_lineups()
+
+  def __repr__(self):
+    ps = '{0}'.format(self.pos)
+    return ps
+
+  def __str__(self):
+    return self.__repr__()
+
+  def change(self):
+    print(self)
+    x = input('choose formation:\n{0}\n'.format('\t'.join(formations))).strip()
+    if x in formations:
+      self.nlist = x
+      self.get_pos()
+      self.get_pos_lineups()
+
+  def get_pos(self):
+    self.pos = [int(i) for i in self.nlist.split('-')]
+
+  def get_pos_lineups(self):
+    i = 2
+    j = self.pos[0] + i
+    self.full_back_lineups = range(i, j)
+    i = j
+    j = self.pos[1] + i
+    self.half_back_lineups = range(i, j)
+    i = j
+    j = self.pos[2] + i
+    self.midfielders_lineups = range(i, j)
+    i = j
+    j = self.pos[3] + i
+    self.half_forward_lineups = range(i, j)
+    i = j
+    j = self.pos[4] + i
+    self.full_forward_lineups = range(i, j)
+
