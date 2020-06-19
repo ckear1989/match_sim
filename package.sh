c97d272ab20e8b4efd3040d30a432dc99bc922d6
ckear1989 <ckear1989@gmail.com>
2020-06-19 10:27:41 +0100

    package

---
+++ package.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# python3 -m pip install --user --upgrade setuptools wheel
+
+# python3 setup.py sdist bdist_wheel
+
+# pip3 install dist/match_sim-ckear-0.0.1.tar.gz --verbose
+
+sudo python3 setup.py install
+
