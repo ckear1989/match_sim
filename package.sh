227935ac9bdac371c8f48a2174146ab7d596c11a
ckear1989 <ckear1989@gmail.com>
2020-06-19 10:33:51 +0100

    rename dir for packaging

--- package.sh
+++ package.sh
@@ -2,9 +2,9 @@
 
 # python3 -m pip install --user --upgrade setuptools wheel
 
-# python3 setup.py sdist bdist_wheel
+# sudo python3 setup.py sdist bdist_wheel
 
-# pip3 install dist/match_sim-ckear-0.0.1.tar.gz --verbose
+pip3 install dist/match_sim-ckear-0.0.1.tar.gz --verbose
 
-sudo python3 setup.py install
+# sudo python3 setup.py install
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
   long_description = fh.read()
 
 setuptools.setup(
-  name="match_sim-ckear", # Replace with your own username
+  name="match_sim-ckear",
   version="0.0.1",
   author="Conor Kearney",
   author_email="ckear@example.com",
