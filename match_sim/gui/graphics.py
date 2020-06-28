6b73cc441445a5b48024667663d911d60ccb007c
ckear1989 <ckear1989@gmail.com>
2020-06-28 14:45:19 +0100

    more pitch markings

--- match_sim/gui/graphics.py
+++ match_sim/gui/graphics.py
@@ -13,10 +13,6 @@ DR = wx.Colour(250, 0, 0)
 BL = wx.Colour(0, 0, 0)
 WH = wx.Colour(255, 255, 255)
 
-def add_player(pitch, color, centre):
-  # pygame.draw.circle(pitch, color, centre, 10)
-  return pitch
-
 class PaintPanel(TemplatePanel):
   def __init__(self, parent):
     super().__init__(parent)
@@ -25,11 +21,11 @@ class PaintPanel(TemplatePanel):
     self.InitUI()
 
   def InitUI(self): 
-    self.Bind(wx.EVT_PAINT, self.OnPaint) 
+    self.Bind(wx.EVT_PAINT, self.GetPitch) 
     self.Centre() 
     self.Show(True)
 
-  def OnPaint(self, event):
+  def GetPitch(self, event):
     dc = wx.PaintDC(self)
     brush = wx.Brush('white')
     dc.Clear()
@@ -42,7 +38,8 @@ class PaintPanel(TemplatePanel):
 		
     pen = wx.Pen(BL)
     dc.SetPen(pen)
-    x, y, n = (340, 520, 5)
+    x, y = (85 * 4, 140 * 4)
+    n = 5
     x0 = 400
     y0 = 100
     for i in range(n):
@@ -52,37 +49,37 @@ class PaintPanel(TemplatePanel):
       dc.DrawRectangle(x0, y0 + (((i*2)+1)*y/n/2), x, y/n/2)
     pen = wx.Pen(WH)
     dc.SetPen(pen)
-    # 6 yard box
-    w = 40
-    h = 10
+    # 6M
+    w = 14 * 4
+    h = 4.5 * 4
     x1 = x0 + (x/2) - (w/2)
     y1 = y0 +1
     dc.DrawLineList([[x1, y1, x1, y1+h], [x1, y1+h, x1+w, y1+h], [x1+w, y1+h, x1+w, y1]])
     y1 = y0 + y -  2
-    h = h*-1
+    h = h * -1
     dc.DrawLineList([[x1, y1, x1, y1+h], [x1, y1+h, x1+w, y1+h], [x1+w, y1+h, x1+w, y1]])
     # penalty box
-    w = 60
-    h = 30
+    w = 19 * 4
+    h = 13 * 4
     x2 = x0 +(x/2) - (w/2)
     y2 = y0 + 1
     dc.DrawLineList([[x2, y2, x2, y2+h], [x2+w, y2, x2+w, y2+h]])
     y2 = y0 + y - 2
     h = h*-1
     dc.DrawLineList([[x2, y2, x2, y2+h], [x2+w, y2, x2+w, y2+h]])
-    # 14
+    # 13M
     x3 = x0 + 1
-    h = 31
+    h = 13 * 4
     w = x - 3
     y3 = y0 + h
-    # 21
-    h = 40
+    # 20
+    h = 20 * 4
     y4 = y0 + h
     # 45
-    h = 120
+    h = 45 * 4
     y5 = y0 + h
     # 65
-    h = 160
+    h = 65 * 4
     y6 = y0 + h
     dc.DrawLineList([
       [x3, y3, x3+w, y3],
@@ -90,20 +87,15 @@ class PaintPanel(TemplatePanel):
       [x3, y5, x3+w, y5],
       [x3, y6, x3+w, y6]
     ])
-    # 14
-    x3 = x0 + 1
-    h = 31
-    w = x - 3
-    y3 = y0 + y - h
-    # 21
-    h = 40
-    y4 = y0 + y - h
-    # 45
-    h = 120
-    y5 = y0 + y - h
-    # 65
-    h = 160
-    y6 = y0 + y - h
+    # mirror
+    h = 13 * 4
+    y3 = y0 + y - h - 2
+    h = 20 * 4
+    y4 = y0 + y - h - 2
+    h = 45 * 4
+    y5 = y0 + y - h - 2
+    h = 65 * 4
+    y6 = y0 + y - h - 2
     dc.DrawLineList([
       [x3, y3, x3+w, y3],
       [x3, y4, x3+w, y4],
@@ -111,10 +103,24 @@ class PaintPanel(TemplatePanel):
       [x3, y6, x3+w, y6]
     ])
     # HW
-    w = 20
+    w = 20 * 4
     x7 = x0 + (x/2) - (w/2)
     y7 = y0 + (y/2)
     dc.DrawLine(x7, y7, x7+w, y7)
+    # semi circles
+    r = 13 * 4
+    x8 = x0 + (x/2) - r
+    y8 = y0 + (20 * 4)
+    dc.DrawArc(x8, y8, x8 + (r*2), y8, x8 + r, y8)
+    y8 = y0 + y - (20 * 4) - 2
+    dc.DrawArc(x8 + (r*2), y8, x8, y8, x8 + r, y8)
+    # penalty spots
+    dc.SetBrush(wx.Brush(WH))
+    x9 = x0 + (x/2)
+    y9 = y0 + (11 * 4)
+    dc.DrawCircle(x9, y9, 3)
+    y10 = y0 + y - (11 * 4)
+    dc.DrawCircle(x9, y10, 3)
 
 class PaintFrame(TemplateFrame):
   def __init__(self):
