02ef1b199fe6c3c8587159c2736cd47faf71b4a6
ckear1989 <ckear1989@gmail.com>
2020-07-07 09:11:54 +0100

    settings panel

--- match_sim/gui/settings.py
+++ match_sim/gui/settings.py
@@ -8,11 +8,30 @@ from match_sim.settings import Settings as ClSettings
 class SettingsPanel(TemplatePanel):
   def __init__(self, parent):
     super().__init__(parent)
-    self.currently_showing = ''
-    self.txt_output.AppendText(str(self.currently_showing))
+    self.game = self.GetParent().game
+    self.txt_output.Destroy()
+    self.autosave = wx.CheckBox(self, label='Autosave')
+    self.vbox1 = wx.BoxSizer(wx.VERTICAL)
+    self.vbox1.Add(self.autosave, flag=wx.ALL, border=10)
+    self.match_speed = wx.Slider(self, minValue=1, maxValue=70, style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+    self.vbox1.Add(wx.StaticText(self, label='Match Speed'), flag=wx.EXPAND|wx.LEFT|wx.TOP|wx.RIGHT, border=10)
+    self.vbox1.Add(self.match_speed, flag=wx.EXPAND|wx.LEFT|wx.BOTTOM|wx.RIGHT, border=10)
+    self.Bind(wx.EVT_CHECKBOX, self.checkbox)
+    self.Bind(wx.EVT_SLIDER, self.slider)
+    self.set_current()
+    self.hbox1.Add(self.vbox1)
     self.SetSizer(self.main_sizer)
 
+  def set_current(self):
+    self.autosave.SetValue(self.game.settings.autosave)
+    self.match_speed.SetValue(self.game.settings.match_speed)
+
+  def checkbox(self, event):
+    self.game.settings.autosave = self.autosave.IsChecked()
+
+  def slider(self, event):
+    self.game.settings.match_speed = self.match_speed.GetValue()
+
 class Settings(ClSettings):
-  '''Create object to store game settings populated with defaults'''
   def __init__(self):
-    super().__init__()
+    super().__init__(self)
