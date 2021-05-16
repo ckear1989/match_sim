e199e71da64dca9d9d947c59e2a5ed1672e82d67
ckear1989 <ckear1989@gmail.com>
2021-05-16 22:04:00 +0100

    test cases

--- match_sim/tests/test_events.py
+++ match_sim/tests/test_events.py
@@ -17,39 +17,42 @@ def team_a():
 def team_b():
   return MatchTeam(Team('b', 'b'), 'b')
 
-def run_event(i, team_a, team_b):
+@pytest.fixture
+def match(team_a, team_b):
+  return Match(team_a, team_b, datetime.date(2020, 1, 1), True, False)
+
+def run_event(i, team_a, team_b, match):
   random.seed(i)
-  match = Match(team_a, team_b, datetime.date(2020, 1, 1), True, False)
   event = Event(match)
   event.run(match)
 
-def test_event(team_a, team_b):
-  run_event(123, team_a, team_b)
-  run_event(123, team_a, team_b)
+def test_event(team_a, team_b, match):
+  run_event(123, team_a, team_b, match)
+  run_event(123, team_a, team_b, match)
 
-def test_event_posession(team_a, team_b):
+def test_event_posession(team_a, team_b, match):
   team_a.tactics_change('posession')
   team_b.tactics_change('posession')
-  run_event(123, team_a, team_b)
-  run_event(123, team_a, team_b)
+  run_event(123, team_a, team_b, match)
+  run_event(123, team_a, team_b, match)
 
-def test_event_blanket(team_a, team_b):
+def test_event_blanket(team_a, team_b, match):
   team_a.tactics_change('blanket')
   team_b.tactics_change('blanket')
-  run_event(123, team_a, team_b)
-  run_event(123, team_a, team_b)
+  run_event(123, team_a, team_b, match)
+  run_event(123, team_a, team_b, match)
 
-def test_event_attacking(team_a, team_b):
+def test_event_attacking(team_a, team_b, match):
   team_a.tactics_change('attacking')
   team_b.tactics_change('attacking')
-  run_event(123, team_a, team_b)
-  run_event(123, team_a, team_b)
+  run_event(123, team_a, team_b, match)
+  run_event(123, team_a, team_b, match)
 
-def test_event_gung_ho(team_a, team_b):
+def test_event_gung_ho(team_a, team_b, match):
   team_a.tactics_change('gung-ho')
   team_b.tactics_change('gung-ho')
-  run_event(123, team_a, team_b)
-  run_event(123, team_a, team_b)
+  run_event(123, team_a, team_b, match)
+  run_event(123, team_a, team_b, match)
 
 def test_event_send_off(team_a, team_b):
   team_a.send_off_player(team_a.playing[0])
@@ -57,11 +60,25 @@ def test_event_send_off(team_a, team_b):
   assert len(team_a.playing) == 14
   assert len(team_b.playing) == 14
 
-def test_event_send_off(team_a, team_b):
+def test_event_sub_1(team_a, team_b):
   team_a.auto_sub(team_a.playing[0])
   team_b.auto_sub(team_b.playing[0])
   assert len(team_a.playing) == 15
   assert len(team_b.playing) == 15
   assert team_a.subs_used == 1
   assert team_b.subs_used == 1
+
+def test_event_sub_2(team_a, team_b):
+  player_a = team_a.playing[0]
+  player_b = team_b.playing[0]
+  assert team_a.subs_used == 0
+  assert team_b.subs_used == 0
+  team_a.auto_sub(player_a)
+  team_b.auto_sub(player_b)
+  assert len(team_a.playing) == 15
+  assert len(team_b.playing) == 15
+  assert team_a.subs_used == 1
+  assert team_b.subs_used == 1
+  assert player_a not in team_a.playing
+  assert player_b not in team_b.playing
 
